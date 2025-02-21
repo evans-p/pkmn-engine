06db4f96f77ce709b4adc3d37691b52b85db4ac2
poulakise <evanspoulaki@gmail.com>
2025-02-21 22:29:21 +0200

    feat(Nature): Added Nature enum.

---
+++ src/main/java/gr/evansp/Nature.java
@@ -0,0 +1,32 @@
+package gr.evansp;
+
+/**
+ * Enum Representing Pokemon natures.
+ */
+public enum Nature {
+	HARDY,
+	LONELY,
+	ADAMANT,
+	NAUGHTY,
+	BRAVE,
+	BOLD,
+	DOCILE,
+	IMPISH,
+	LAX,
+	RELAXED,
+	MODEST,
+	MILD,
+	BASHFUL,
+	RASH,
+	QUIET,
+	CALM,
+	GENTLE,
+	CAREFUL,
+	QUIRKY,
+	SASSY,
+	TIMID,
+	HASTY,
+	JOLLY,
+	NAIVE,
+	SERIOUS
+}
